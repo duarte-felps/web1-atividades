f3c06df614f5345bfd487435723525a81d3dc15f
duarte-felps <felipedsilva9@hotmail.com>
2021-04-26 19:34:49 -0400

    first commit

--- ex10/ex10.js
+++ ex10/ex10.js
@@ -1,4 +1,3 @@
 /* 
   10.	 Crie um programa para controle das despesas onde ele deve receber como entrada do usuário a descrição e o valor da despesa que devem ser armazenados em um vetor.
    O programa deve, cada inclusão de conta, listar para o usuário as contas já cadastradas e exibir um totalizador" aria-hidden="true"></i>*/
-   AbortController
